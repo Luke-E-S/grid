1f01632e5a313862b0010e28e6d21034ed18b3e0
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-11 16:01:38 +0100

    chore: put grid creation into a function

--- script.js
+++ script.js
@@ -10,28 +10,33 @@ const gridSize = gridWidth * gridWidth;
 
 container.style.width = containerWidth + "px";
 
-/*  create the grid of divs, give them a class for style */
-for(let i = 1; i < gridSize+1; i++) {
-    const div = document.createElement("div");
-    div.className = "grid";
+function createGrid(width, size) {
+        /*  create the grid of divs, give them a class for style */
+    for(let i = 1; i < size+1; i++) {
+        const div = document.createElement("div");
+        div.className = "grid";
+        
+        /* display borders without any sides touching eachother */
+        div.style.borderTop = "1px solid black";
+        div.style.borderLeft = "1px solid black";
     
-    /* display borders without any sides touching eachother */
-    div.style.borderTop = "1px solid black";
-    div.style.borderLeft = "1px solid black";
-   
-    if (i > gridSize - gridWidth) /* give bottom of grid a border */
-    {
-        div.style.borderBottom = "1px solid black";
-    }
-    if(i % gridWidth === 0) /* give right side a border */
-    {
-        div.style.borderRight = "1px solid black";
-    }
-    div.style.width =  containerWidth / gridWidth + "px";
-    div.style.height =  containerWidth / gridWidth + "px";
-    container.appendChild(div);
+        if (i > size - width) /* give bottom a border */
+        {
+            div.style.borderBottom = "1px solid black";
+        }
+        if(i % width === 0) /* give right side a border */
+        {
+            div.style.borderRight = "1px solid black";
+        }
+        div.style.width =  containerWidth / width + "px";
+        div.style.height =  containerWidth / width + "px";
+        container.appendChild(div);
 
+    }
 }
+
+createGrid(gridWidth, gridSize);
+
 
 /* array used to add events tot he grid for painting */
 const gridItem = document.getElementsByClassName("grid");
