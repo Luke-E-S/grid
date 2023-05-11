52a1a3d79e6fbb1a70ac15c23d4471e19df69c4f
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-11 15:55:00 +0100

    chore: cleaned up border creation code

--- script.js
+++ script.js
@@ -10,20 +10,22 @@ const gridSize = gridWidth * gridWidth;
 
 container.style.width = containerWidth + "px";
 
+/*  create the grid of divs, give them a class for style */
 for(let i = 1; i < gridSize+1; i++) {
     const div = document.createElement("div");
     div.className = "grid";
+    
+    /* display borders without any sides touching eachother */
     div.style.borderTop = "1px solid black";
     div.style.borderLeft = "1px solid black";
-    if(i===gridSize) {
-        div.style.borderRight = "1px solid black";
+   
+    if (i > gridSize - gridWidth) /* give bottom of grid a border */
+    {
         div.style.borderBottom = "1px solid black";
     }
-    else if(i % gridWidth === 0) {
+    if(i % gridWidth === 0) /* give right side a border */
+    {
         div.style.borderRight = "1px solid black";
-    }
-    else if (i > gridSize - gridWidth) {
-        div.style.borderBottom = "1px solid black";
     }
     div.style.width =  containerWidth / gridWidth + "px";
     div.style.height =  containerWidth / gridWidth + "px";
@@ -31,9 +33,8 @@ for(let i = 1; i < gridSize+1; i++) {
 
 }
 
+/* array used to add events tot he grid for painting */
 const gridItem = document.getElementsByClassName("grid");
-
-
 for(let i = 0; i < gridItem.length; i++){
     gridItem[i].addEventListener("mouseover", 
     (() => gridItem[i].style.backgroundColor = "blue"))
