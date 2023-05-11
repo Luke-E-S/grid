5b932decff60ef07707461ca04ee3fdb61a6749a
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-11 13:43:39 +0100

    feat: grid of divs added to container using loop

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const container = document.querySelector(".container");
+
+for(let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.className = "grid";
+    container.appendChild(div);
+
+}
