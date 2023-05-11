460d78005496df4e9cd6a839bcf9466ed23a94f6
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-11 14:24:36 +0100

    feat: grid size adjustable and fits in same space

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 256; i++) {
+const defaultGridWidth = 16;
+const containerWidth = 960;
+const gridSize = defaultGridWidth * defaultGridWidth;
+
+container.style.width = containerWidth + "px";
+
+for(let i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
     div.className = "grid";
+    div.style.width =  containerWidth / defaultGridWidth + "px";
+    div.style.height =  containerWidth / defaultGridWidth + "px";
     container.appendChild(div);
 
 }
