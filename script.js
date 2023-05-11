a8f7a77700a66000c5dcd14f2241053657cee759
Luke-E-S <sadface.ke2uke@gmail.com>
2023-05-11 15:39:49 +0100

    feat: removed div numbers that were used for testing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const adjustSizeButton = document.createElement("button");
 adjustSizeButton.textContent = "Adjust Grid Size";
 container.parentElement.insertBefore(adjustSizeButton, container);
 
-const gridWidth = 16;
+const gridWidth = 64;
 const containerWidth = 960;
 const gridSize = gridWidth * gridWidth;
 
@@ -12,7 +12,6 @@ container.style.width = containerWidth + "px";
 
 for(let i = 1; i < gridSize+1; i++) {
     const div = document.createElement("div");
-    div.textContent = i;
     div.className = "grid";
     div.style.borderTop = "1px solid black";
     div.style.borderLeft = "1px solid black";
@@ -20,7 +19,7 @@ for(let i = 1; i < gridSize+1; i++) {
         div.style.borderRight = "1px solid black";
         div.style.borderBottom = "1px solid black";
     }
-    else if(i % 16 === 0) {
+    else if(i % gridWidth === 0) {
         div.style.borderRight = "1px solid black";
     }
     else if (i > gridSize - gridWidth) {
